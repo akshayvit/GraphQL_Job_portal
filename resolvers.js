db826d02176d30e663362f5e40e0dd0db3d4f1b3
Akshay Chatterjee <akshaychatterjee@Akshays-MacBook-Pro.local>
2023-05-16 19:56:21 +0530

    Mutation added

--- resolvers.js
+++ resolvers.js
@@ -1,12 +1,25 @@
 import { Company, Job } from "./db.js";
 
+let Jobs=[]
+
 export const resolvers= {
     Query: {
         jobs: ()=> Job.findAll(),
-        job: (_root,{id})=>Job.findById(id)
+        job: (_root,{id})=>Job.findById(id),
+        jobs_pushed: ()=>Job.findAll()
     },
+
+    Mutation: {
+        createJob:  (_root,{title,descrp},context)=>{
+            return Job.create({
+               id: "112",
+               title:title,
+               description: descrp
+            })
+        }
+    },
+
     Job: {
-        id: Job.findById(Job.id)
         
     }
 };
